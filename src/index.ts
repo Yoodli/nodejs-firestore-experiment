1954ffd9ec74f813d0d1e7f6b7d572edd6e8e0c1
Hiroshi Ohno <hiroshi@yoodli.ai>
2024-07-20 07:41:53 -0700

    An experiment that works

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,24 @@
-// eslint-disable-next-line no-console
-console.log('Hello world!');
+/* eslint-disable no-console */
+
+import { AggregateField, Firestore } from '@google-cloud/firestore';
+
+async function main() {
+  const firestore = new Firestore();
+
+  const rev1 = firestore.doc('experiments/1');
+  const rev2 = firestore.doc('experiments/2');
+  await rev1.set({
+    x: 10,
+    y: 20,
+  });
+  await rev2.set({
+    x: 30,
+    y: 90,
+  });
+  const col = firestore.collection('experiments');
+  const sumAgg = col.aggregate({ total: AggregateField.sum('y') });
+  const snapshot = await sumAgg.get();
+  console.log(snapshot.data().total);
+}
+
+main().catch(console.error);
